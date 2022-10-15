d24cd39d8ff5c2dce67007cf937dd8c62352806e
IsRadoIsChibs <coloradochibs@gmail.com>
2022-10-15 17:25:30 +0000

    sour betty

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
- *Main- prints phrase "with proper grammar, but the outcome is a piece of art,", 
-*return : 0
-*/
-
+ * main - entry point
+ * Description: prints "with proper grammar,
+ * but the outcome is a piece of art,
+ *
+ * Return: is always 0
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
