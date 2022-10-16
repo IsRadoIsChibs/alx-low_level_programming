4ab1a7340489658b794dbd6073b88387ecca54c9
IsRadoIsChibs <coloradochibs@gmail.com>
2022-10-16 20:29:48 +0000

    3

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* Description - prints lowercase alphabets
+* Return: always 0
+*/
+int main(void)
+{
+	char ap = 'a';
+
+	for (ap = 'a'; ap <= 'z'; ap++)
+		putchar (ap\n);
+
+	return (0);
+}
