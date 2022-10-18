9fb256f39c7faf27633436a68e458c9f911ac320
IsRadoIsChibs <coloradochibs@gmail.com>
2022-10-18 15:52:50 +0000

    m

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+* Description - prints to standard error
+*
+* Return: 1 (success)
+*/
+int main(void)
+{
+	fprintf(stderr, "%s", "and that piece of art is useful\"");
+	fprintf(stderr, "%s", "- Dora Korpar, 2015-10-19\n");
+	return (1);
+
+}
