c955fba729a6414d4af7f4335c6cea5a142adbe5
IsRadoIsChibs <coloradochibs@gmail.com>
2022-10-19 20:21:39 +0000

    l

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* Description - prints numbers of base 16 lowercase
+* Return: always 0
+*/
+int main(void)
+{
+	int p;
+	char c;
+
+		for (p= 48; p < 58; p++)
+		{
+			putchar(p);
+		
+		}
+		for (c = 'a'; c <= 'f'; c++)
+		{
+			putchar(c);
+		}
+		putchar('\n');
+
+		return (0);
+}
