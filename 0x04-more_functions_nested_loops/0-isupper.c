20a22436903af6f441e12309473674f9fd1050b8
IsRadoIsChibs <coloradochibs@gmail.com>
2022-10-21 12:28:50 +0000

    try2

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+  * _isupper - checks for uppercase character
+  * @c: input character  that is checked
+  *
+  * Return: 1 if uppercase 0 otherwise
+  */
+int _isupper(int c)
+{
+
+	return (isupper(&c));
+
+
+}
