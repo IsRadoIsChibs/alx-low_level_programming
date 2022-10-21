e394c1fa5b2099e1f7dfb29f036b3cdb5226548a
IsRadoIsChibs <coloradochibs@gmail.com>
2022-10-21 20:16:09 +0000

    pmn

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * print_numbers-- print number 0-9 sxcept 2 and 4
+  * Return: void*
+  */
+void print_most_numbers(void)
+{
+	int c = '0';
+
+	while ( c <= '9')
+	{
+	if ((c != '2') && (c != '4'))
+
+	_putchar (c);
+
+	c++;
+	}
+	_putchar('\n');
+}
