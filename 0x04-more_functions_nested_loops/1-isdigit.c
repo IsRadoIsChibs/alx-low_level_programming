f4a6a4cf9a7a6cfc455286ec80f70a7de14aa0b7
IsRadoIsChibs <coloradochibs@gmail.com>
2022-10-21 13:52:09 +0000

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+  * _isdigit - checks for uppercase character
+  * @c: input character  that is checked
+  *
+  * Return: 1 if C is a digit otherwise 0
+  */
+int _isdigit(int c)
+{
+	if
+	 isdigit(c)
+	return (1);
+	else
+	return (0);
+
+
+}
