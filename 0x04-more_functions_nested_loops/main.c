e17352970ae256695b36fc1e4d5c529068e9b69d
IsRadoIsChibs <coloradochibs@gmail.com>
2022-10-23 18:19:49 +0000

    Nests

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -1,5 +1,15 @@
 #include "main.h"
 
-
-int main ()
-
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_line(0);
+	print_line(2);
+	print_line(10);
+	print_line(-4);
+	return (0);
+}
