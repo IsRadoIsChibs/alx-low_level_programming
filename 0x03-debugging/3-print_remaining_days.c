025361470ac7a76968ea2cc50d4aa3950b0748d2
IsRadoIsChibs <coloradochibs@gmail.com>
2022-10-23 15:11:09 +0000

    ILD

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,7 +12,7 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
+	if ((year % 4 != 0 || year % 400 != 0) && (year % 100 != 0))
 	{
 		if (month >= 2 && day >= 60)
 		{
@@ -24,10 +24,12 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if (month == 2 && day == 60)
+		if ((month == 2 && day == 60) && (year % 4 == 0))
 		{
+			day++;
+		
+		}
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
-		}
 	}
-}
+
