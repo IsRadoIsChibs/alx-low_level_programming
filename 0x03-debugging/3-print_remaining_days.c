5c599c076fd232422ab52c2dabfac68d630deb87
IsRadoIsChibs <coloradochibs@gmail.com>
2022-10-23 15:18:49 +0000

    Do I?

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -27,9 +27,9 @@ void print_remaining_days(int month, int day, int year)
 		if ((month == 2 && day == 60) && (year % 4 == 0))
 		{
 			day++;
-		
+
 		}
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 	}
-
+}
