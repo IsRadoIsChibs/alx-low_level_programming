9a2feac08d1c45727083f24ec7ae8f51056df6b3
IsRadoIsChibs <coloradochibs@gmail.com>
2022-10-26 13:09:15 +0000

    length of string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strlen - determines length of string
+ * @s: pointer to a string
+ *
+ *Return: length of sring
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
