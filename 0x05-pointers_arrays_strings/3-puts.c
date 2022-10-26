c2c8df154eae3bcc8dbec825f29f13b40c5e5111
IsRadoIsChibs <coloradochibs@gmail.com>
2022-10-26 13:26:56 +0000

    prototype put function for strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts - prints string to standard output
+ * @str - string
+ * 
+ * return - returns string
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
