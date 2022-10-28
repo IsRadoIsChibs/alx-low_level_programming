46f80af7e1b7aff57b1dc99725ac6f748e85b55f
IsRadoIsChibs <coloradochibs@gmail.com>
2022-10-28 15:25:29 +0000

    standard fun

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 
 /**
+ * _strcmp - compares two strings
+ * @s1: string input
+ * @s2: string input
  *
- *
- *
- *
+ * Return: returns 0, negative or postive int
  */
 
 int _strcmp(char *s1, char *s2)
