e58954adf85a3c4aab701252553b222d1793c531
IsRadoIsChibs <coloradochibs@gmail.com>
2022-11-08 12:21:20 +0000

    m

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - multiply tw0 numbers
+ * @argc:  argument count int
+ * @argv: argument vector string
+ *
+ * Return:0 success, 1 if error
+ */
+
+int main(int argc, char *argv[])
+{
+	int b;
+	int mul = 1;
+
+	if (argc > 1)
+	{
+		for (b = 1; b < argc; b++)
+		{
+			mul = mul * atoi(argv[b]);
+		}
+		printf("%d\n", mul);
+		return (0);
+	}
+	else
+		printf("Error\n");
+		return (1);
+}
