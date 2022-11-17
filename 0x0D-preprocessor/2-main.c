1a1fa898ae9041f7fb645285bda7e99cf6a05ce6
IsRadoIsChibs <coloradochibs@gmail.com>
2022-11-17 10:54:53 +0000

    Preprocessor tasks, late but done

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description - prints name of this file
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+
+}
