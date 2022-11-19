13d59c8b1378d3520b9bfd0374fc115faf7cc2d4
IsRadoIsChibs <coloradochibs@gmail.com>
2022-11-19 12:44:32 +0000

    c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -10,14 +10,13 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list vlist;
 	unsigned int i;
-	int sum;
+	int sum = 0;
 
 	if (n == 0)
 		return (0);
 
 	va_start(vlist, n);
 
-	sum = 0
 	for (i = 0; i < n; i++)
 	{
 		sum += va_arg(vlist, int);
